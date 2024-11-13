a80a153f88a620c4281a105a9fa1aa2c49c2cce2
frostfairgr-10 <168585026+frostfairgr-10@users.noreply.github.com>
2024-11-13 22:01:26 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ const config = {
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    admin: "truglI53",
+    KYLJames: "ILoveJHTTOHLing69",
   },
 };
 
